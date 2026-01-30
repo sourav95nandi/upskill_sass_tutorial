5bfb8c8eaf3d323201108ee300b80f985662c6ef
sourav95nandi <sourav95nandi@gmail.com>
2026-01-30 09:29:59 +0000

    Added create action to save db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
   def new
-  end  
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: 'Your message has been sent successfully.'
+    else
+      redirect_to new_contact_path, alert: 'There was an error sending your message. Please try again.'
+    end
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :message)
+    end
 end
