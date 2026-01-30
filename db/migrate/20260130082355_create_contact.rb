bbe36a6c7efb0bdf568a10ad2aea2e3ed312773b
sourav95nandi <sourav95nandi@gmail.com>
2026-01-30 08:25:44 +0000

    Databse generated

---
+++ db/migrate/20260130082355_create_contact.rb
@@ -0,0 +1,10 @@
+class CreateContact < ActiveRecord::Migration[8.1]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.text :message
+      t.timestamps
+    end
+  end
+end
