175f466d81c4ad524232381f568c1d893746d81c
sourav95nandi <sourav95nandi@gmail.com>
2026-02-02 12:10:09 +0000

    update contacts controller and mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -18,7 +18,7 @@ class ContactsController < ApplicationController
       flash[:success] = 'We have received your message and will get back to you shortly.'
       redirect_to root_path
     else
-      flash[:danger] = @contact.errors.full_messages.to_sentence
+      flash.now[:danger] = @contact.errors.full_messages.to_sentence
       render :new, status: :unprocessable_entity
     end
   end
@@ -33,7 +33,7 @@ class ContactsController < ApplicationController
       flash[:success] = 'Message updated successfully.'
       redirect_to contacts_path
     else
-      flash[:danger] = @contact.errors.full_messages.to_sentence
+      flash.now[:danger] = @contact.errors.full_messages.to_sentence
       render :edit, status: :unprocessable_entity
     end
   end
