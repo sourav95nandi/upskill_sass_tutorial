784fc84ba8cf6e5fd62867dc62a7eebeb3c6b540
sourav95nandi <sourav95nandi@gmail.com>
2026-02-06 14:13:45 +0000

    removed font-awesome gem

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,9 +1,1 @@
-Rails.application.config.dartsass.builds = {
-  "app.scss" => "app.css"
-}
-
-Rails.application.config.dartsass.build_options = []
-
-if Gem.loaded_specs.key?("font-awesome-sass")
-  Rails.application.config.dartsass.build_options << "--load-path #{Gem.loaded_specs['font-awesome-sass'].full_gem_path}/assets/stylesheets"
-end
+# Dartsass configuration removed as gems were uninstalled.
