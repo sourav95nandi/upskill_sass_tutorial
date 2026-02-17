9566aee445a6c8579c207783f02b3b225d015f76
sourav95nandi <sourav95nandi@gmail.com>
2026-02-17 13:27:45 +0000

    added routes for profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do
+    resource :profile
+  end
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts, except: [:new]
