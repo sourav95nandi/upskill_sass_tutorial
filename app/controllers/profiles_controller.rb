061859f53aebb131be910deee631ca298687a785
sourav95nandi <sourav95nandi@gmail.com>
2026-02-18 11:34:55 +0000

    added users controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,23 @@ class ProfilesController < ApplicationController
     def new
         @profile = Profile.new
     end
+
+    #POST to user/:user_id/profile
+    def create
+        @user=User.find(params[:user_id])
+        @profile = @user.build_profile(profile_params)
+
+        if @profile.save
+            flash[:success] = 'Profile created successfully.'
+            redirect_to root_path
+        else
+            flash.now[:danger] = @profile.errors.full_messages.to_sentence
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,4 @@
 class Profile < ApplicationRecord
   belongs_to :user
-
+  validates :first_name, :last_name, :job_title, :contact_email, :description, presence: true
 end
